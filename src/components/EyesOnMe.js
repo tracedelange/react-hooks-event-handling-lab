0858b9663e3e94e034604e754c95daae1fdb9a18
Trace <tracedelange@me.com>
2021-07-09 09:40:26 -0500

    commit tree c0f077072094b01528d221e60bf63856ba6130cf

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+
+    return (
+
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log("Hey! Eyes on me!")} >Eyes On Me</button>
+
+    )
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+
+const Keypad = () => {
+    return (
+        <input type="password" onChange={ () => console.log('Entering password...') } />
+    )
+}
+
+export default Keypad;
